78729f7e2cd42ff123b15879b9d0c5588b3a093c
JB <163941305+janders1701@users.noreply.github.com>
2024-08-16 20:25:49 -0500

    ghfhgj

--- js/themeChange.js
+++ js/themeChange.js
@@ -1,13 +1,11 @@
-$(document).ready(function () {
-
 $(".themeChange").on("click", function () {
     if ($(this).hasClass("light-mode")) {
-        $("#stylesheet").attr("href", "/CSS/style.css")
+        $("#stylesheet").attr("href", "./CSS/style.css");
+    } elseif ($(this).hasClass("dark-mode")) {
+        $("#stylesheet").attr("href", "./CSS/darkmode.css");
     }
-    else if ($(this).hasClass("dark-mode")) {
-        $("#stylesheet").attr("href", "/CSS/darkmode.css") 
-
-    } 
     console.log($("#stylesheet").attr("href"));
 }); 
-})
+
+
+
