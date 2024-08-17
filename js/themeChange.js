7a1dd02c1d91689ba23784f696ecaa1bd3f54d39
JB <163941305+janders1701@users.noreply.github.com>
2024-08-16 19:57:06 -0500

    update link in jquery

--- js/themeChange.js
+++ js/themeChange.js
@@ -1,9 +1,9 @@
 $(".themeChange").on("click", function () {
     if ($(this).hasClass("light-mode")) {
-        $("#stylesheet").attr("href", "./css/style.css")
+        $("#stylesheet").attr("href", "/josh/css/style.css")
     }
     else if ($(this).hasClass("dark-mode")) {
-        $("#stylesheet").attr("href", "./css/darkmode.css") 
+        $("#stylesheet").attr("href", "/josh/css/darkmode.css") 
 
     } 
     console.log($("#stylesheet").attr("href"));
