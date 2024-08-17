70a465415d03dd987c41512f02252dd113de649f
JB <163941305+janders1701@users.noreply.github.com>
2024-08-16 20:22:00 -0500

    ?

--- js/themeChange.js
+++ js/themeChange.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-    $(".themeChange").on("click", function ()) {
-        if ($(this).hasClass("light-mode")) {
-            $("#stylesheet").attr("href", "/CSS/style.css");
-        }
-        elseif ($(this).hasClass("dark-mode")) {
-            $("#stylesheet").attr("href", "/CSS/darkmode.css");
-        }
-        console.log($("#stylesheet").attr("href"));
+
+$(".themeChange").on("click", function () {
+    if ($(this).hasClass("light-mode")) {
+        $("#stylesheet").attr("href", "/CSS/style.css")
     }
-    });
+    else if ($(this).hasClass("dark-mode")) {
+        $("#stylesheet").attr("href", "/CSS/darkmode.css") 
+
+    } 
+    console.log($("#stylesheet").attr("href"));
+}); 
+})
