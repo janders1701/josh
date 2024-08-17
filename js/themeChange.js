c9c1830e6e37ef3b88d31fe79fee564c96c8fcf7
JB <163941305+janders1701@users.noreply.github.com>
2024-08-16 19:50:46 -0500

    back

--- js/themeChange.js
+++ js/themeChange.js
@@ -1,8 +1,8 @@
 $(".themeChange").on("click", function () {
-    if ($(this).hasClass("lightmode")) {
+    if ($(this).hasClass("light-mode")) {
         $("#stylesheet").attr("href", "./css/style.css")
     }
-    else if ($(this).hasClass("darkmode")) {
+    else if ($(this).hasClass("dark-mode")) {
         $("#stylesheet").attr("href", "./css/darkmode.css") 
 
     } 
